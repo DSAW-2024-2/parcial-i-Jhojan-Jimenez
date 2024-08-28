fec57ada9d6a98ef2e1db26e4c8a9ecbae51c243
Jhojan Jimenez <jhojanjimene@gmail.com>
2024-08-28 09:06:08 -0500

    First Try

---
+++ index.js
@@ -0,0 +1,5 @@
+document.getElementById("share-button").addEventListener("click", function () {
+  var menu = document.getElementById("share-menu");
+  menu.classList.toggle("visible");
+  menu.classList.toggle("hidden");
+});
